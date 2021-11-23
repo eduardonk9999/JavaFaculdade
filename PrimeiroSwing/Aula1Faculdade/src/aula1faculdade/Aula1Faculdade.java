6554e9724209a13bcf9852cd14fbefbd7d1f7132
eduardonk9999 <thiago.novak7@gmail.com>
2021-11-23 19:24:25 -0300

    Add, aula 1

---
+++ PrimeiroSwing/Aula1Faculdade/src/aula1faculdade/Aula1Faculdade.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula1faculdade;
+
+/**
+ *
+ * @author ganda
+ */
+public class Aula1Faculdade {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int inteiro = 29;
+        char caracter = 'E';
+        double real = 1.77;
+        String frase = "Eduardo";
+        boolean VF = true;
+        if(VF == true) {
+            System.out.println("Eu sou o " + frase + "tenho" + inteiro + "anos de idade" + caracter + real "de altura" )
+        }
+    }
+    
+}
